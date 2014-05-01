12cd0d448e89828a3ab0da4da65d9e90f9ced3f0
Stephen Scaff <stephen.scaff@sosweetcreative.com>
2014-05-01 11:29:55 -0400

    Clean up
    Chained fadeInTime, removed sloppy stuff

--- chickendinner.js
+++ chickendinner.js
@@ -29,52 +29,37 @@
 *
 */
 
-
 (function($){
   $.chickenDinner = {
     defaults: {
-      altTag: ['Banner Image'],
-      fadeInTime:2000,
-      path: 'images/',
-      TheImages: ['ban1.png', 'ban2.png', 'ban3.png', 'ban4.png', 'ban5.png' ] 			
-    }			
+      altTag: ['Banner Image manssss'],
+      fadeInTime:1800,
+      TheImages: ['banner1.png', 'banner2.png', 'banner3.png', 'banner4.png', 'banner5.png' ]
+    }
   };
-
-$.fn.extend({
-  chickenDinner:function(options) {
-
-    var options =  $.extend({}, $.chickenDinner.defaults, options); 
-
-    return this.each(function() {
-      var self = this;
-      var TheImages = options.TheImages;
-      var RandomMath = Math.floor(Math.random()*TheImages.length);
-      var SelectedImage = TheImages[RandomMath];
-      var imgPath = options.path + SelectedImage;
-      var altTag = options.altTag;
-
-      // Fade in Times
-      var fadeInTime = options.fadeInTime;
-
-      //Fade In animation - hide first
-      $(this).css('display', 'none');
-      $(this).fadeIn(fadeInTime);
-
-      //For background-image: If cssBG is True
-      if(options.cssBG == 'true'){
-
-        $(this).css('background-image', 'url(' + imgPath + ')');
-
-      } else{
-
-        $(this).attr( {
-		      src: imgPath,
-		      alt: altTag
-        });
-      }	
-    });	
-  }
-});
+  
+  $.fn.extend({
+    chickenDinner:function(options) {
+      $.extend({}, $.chickenDinner.defaults, options);
+      return this.each(function() {
+        var TheImages = options.TheImages;
+        var RandomMath = Math.floor(Math.random()*TheImages.length);
+        var SelectedImage = TheImages[RandomMath];
+        var imgPath = options.path + SelectedImage;
+        var altTag = options.altTag;
+        // Fade in Times
+        var fadeInTime = options.fadeInTime;
+        //Fade In animation - hide first
+        $(this).css('display', 'none').fadeIn(fadeInTime);
+        if(options.cssBG == 'true'){
+          $(this).css('background-image', 'url(' + imgPath + ')');
+        } else{
+          $(this).attr( {
+            src: imgPath,
+            alt: altTag
+          });
+        }
+      });
+    }
+  });
 })(jQuery);
-
-
